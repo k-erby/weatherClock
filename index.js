e482f705383f8c6d77195b2b305b721792bf406a
Kaitlin <cmykaitlin@gmail.com>
2019-02-18 13:59:04 -0800

    Add api call to get degrees of Victoria

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
+// Part of the zeit-script to get it running
 module.exports = (req, res) => {
-	  res.end(`Hello from Node.js on Now 2.0!`);
+	  res.end(`Basic weather clock server stuff`);
 }
 
+const request = require('request');
+
+// Grab apiKey from bash/zsh
+let apiKey = process.env.WEATHER_API;
+let cityID = '6174041';
+
+// Default temp is set to Kelvin, so you have to add the conversion to metric
+let url = `http://api.openweathermap.org/data/2.5/weather?id=${cityID}&units=metric&appid=${apiKey}`;
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log('error:', error);
+  } else {
+    let weather = JSON.parse(body)
+    let message = `It is ${weather.main.temp} degrees in ${weather.name}!`;
+
+    console.log(message);
+  }
+});
