adcf19ca54231999be655b7b142a6fb43a42cea3
Kaitlin <cmykaitlin@gmail.com>
2019-02-20 16:56:34 -0800

    Fuck yeah, axios

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
 const express = require('express');
-const fetch = require('node-fetch');
+const axios = require('axios');
 
 const app = express();
 const port = 4000;
 
 // TODO: this will result in 'undefined' when deployed, so fix that.
 let apiKey = process.env.WEATHER_API;
-//console.log(apiKey)
 let cityID = '6174041';
+let weather;
 
-async function getWeather() {
-    // Default temp is set to Kelvin, so you have to add the conversion to metric
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?id=${cityID}&units=metric&appid=${apiUrl}`;
-    let weather;
+const getWeather = async () => {
+  try {
+    return await axios.get(`http://api.openweathermap.org/data/2.5/weather?id=${cityID}&units=metric&appid=${apiKey}`)
+  } catch (error) {
+    console.error(error)
+  }
+}
 
-    const response = await fetch(apiUrl);
-    return response.json();
+const getCurrentLocalWeather = async () => {
+  const weather = await getWeather();
+
+  if (weather.data) {
+    return weather.data;
+  }
 }
 
 // TODO: zeit is still unhappy with this and throws a 502.
 app.get("/", (req, res) => {
-  getWeather().then(weather => {
-    res.send(weather.main);
-  });
+  getCurrentLocalWeather().then(weatherData => {
+    res.send(weatherData.main)
+  })
 });
+
 const server = app.listen(port);
