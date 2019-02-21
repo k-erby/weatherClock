a44849f3c9b0e94c297dda2c354dfbb4109d4170
Kaitlin <cmykaitlin@gmail.com>
2019-02-20 16:40:30 -0800

    Commit node-fetch way

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require('express');
-const fetch = require("node-fetch");
+const fetch = require('node-fetch');
 
 const app = express();
 const port = 4000;
 
 // TODO: this will result in 'undefined' when deployed, so fix that.
 let apiKey = process.env.WEATHER_API;
+//console.log(apiKey)
 let cityID = '6174041';
 
 async function getWeather() {
     // Default temp is set to Kelvin, so you have to add the conversion to metric
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?id=${cityID}&units=metric&appid=${apiKey}`;
+    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?id=${cityID}&units=metric&appid=${apiUrl}`;
     let weather;
 
     const response = await fetch(apiUrl);
@@ -20,14 +21,7 @@ async function getWeather() {
 // TODO: zeit is still unhappy with this and throws a 502.
 app.get("/", (req, res) => {
   getWeather().then(weather => {
-    res.json(weather);
+    res.send(weather.main);
   });
 });
-
-//app.get("/", (req, res) => {
-//  getWeather()
-//    .then(weather => { console.log(weather); })
-//    .catch(error => { console.log("Butts") });
-//});
-
 const server = app.listen(port);
